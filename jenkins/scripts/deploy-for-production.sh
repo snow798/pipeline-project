564b3247c77f340fe1278c711b2cf35a670a6cb1
luojhua <374864125@qq.com>
2019-09-11 10:36:56 +0800

    Update deploy-for-production.sh
    454

--- jenkins/scripts/deploy-for-production.sh
+++ jenkins/scripts/deploy-for-production.sh
@@ -8,13 +8,6 @@ set -x
 npm install serve
 set +x
 
-echo 'push code... 77777777777777777777 prod'
-set -x
-ssh luo@192.168.117.134
-scp -r luo@192.168.117.134:/var/jenkins_data/workspace/pipeline-project_production/build/ /home/luo/jenkins_res/
-set +x
-echo 'push end'
-
 set -x
 ./node_modules/serve/bin/serve.js -c 0 -s build &
 sleep 1
@@ -23,5 +16,3 @@ set +x
 
 echo 'Now...'
 echo 'Visit http://localhost:5000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 5000:5000''" parameter when you'
-echo 'created your initial Pipeline as a Jenkinsfile.)'
